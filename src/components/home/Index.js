4991143b326b50770c00e59aa7fa8311634465e0
Szilamer Balogh <balogh.szilamer@gmail.com>
2023-06-01 14:12:45 +0300

    Slider working

--- src/components/home/Index.js
+++ src/components/home/Index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import Slider from './Slider'
-
+// import Slider from './Slider'
+import HookSlider from './HookSlider'
+import { SliderData } from './SliderData';
 class Index extends Component {
   render() {
     return (
-      <div className = 'index_page'><Slider /></div>
+      <div className = 'index_page'><HookSlider /></div>
     )
   }
 }
--- src/components/home/Slider.js
+++ src/components/home/Slider.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react'
-import { SliderData } from './SliderData'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+// import React, { Component } from 'react'
+// import { SliderData } from './SliderData'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
-class Slider extends Component {
-  render() {
-    return (
-      <div className='slider_container'>
-        <div className = 'slider-arrows'>
-          <span><FontAwesomeIcon icon={ faChevronLeft } /></span> 
-          <span><FontAwesomeIcon icon={ faChevronRight } /></span></div>
-        {SliderData.map((slide, index) => {  //index-> iteration counter
-            return(<div className =  {index == 0 ? 'active slider' : 'slider'}    key = {index}><img  src = {slide.image} alt = {slide.alt}/></div>);
-        })}
-        </div>
-    )
-  }
-}
+// class Slider extends Component {
+//   constructor(props) {
+//     super(props)
+  
+//     this.state = {
+//        nextImage:0
+//     }
+//   }
+  
 
-export default Slider
+//   // changeSliderRight() {
+//   //   this.setState({
+//   //     nextImage: this.state.nextImage + 1
+//   //   },
+//   //   () =>{
+//   //     // if()
+//   //   })
+    
+//   // }
+
+//   // changeSliderLeft() {
+//   //   this.setState({
+//   //     nextImage: this.state.nextImage - 1
+
+//   //   })
+//   // }
+//   render() {
+//     const sliderLength = SliderData.length;
+
+//     return (
+//       <div className='slider_container'>
+//         <div className = 'slider-arrows'>
+//           <span><FontAwesomeIcon icon={ faChevronLeft } onClick = {() => this.changeSliderLeft()}/></span> 
+//           <span><FontAwesomeIcon icon={ faChevronRight } onClick = {() => this.changeSliderRight()}/></span></div>
+//         {SliderData.map((slide, index) => {  //index-> iteration counter
+//             return(<div className =  {index == 0 ? 'active slider' : 'slider'}    key = {index}><img  src = {slide.image} alt = {slide.alt}/></div>);
+//         })}
+//         </div>
+//     )
+//   }
+// }
+
+// export default Slider
